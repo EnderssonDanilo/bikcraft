762876786bf6ccd132e7e40a55e9ebaf3f824a58
Endersson Danilo <endersondanilo@gmail.com>
2022-08-15 13:24:39 -0300

    adicionado função de ativar itens do orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Ativar link do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
